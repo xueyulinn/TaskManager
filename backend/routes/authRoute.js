a2bfb98c3c8405a6102320b7a4e995a9fba1902f
xueyulinn <xueyulin2001@163.com>
2025-06-11 19:54:57 +0800

    add image upload api using Multer middleware

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,7 +1,18 @@
 import express from "express";
 import { userSignin, userSignup } from "../controllers/authController.js";
+import upload from "../middlewares/uploadMiddleWare.js";
 const router = express.Router();
 
 router.post("/signup", userSignup);
 router.post("/signin", userSignin);
+
+router.post("/upload-image", upload.single("image"), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ message: "No file uploaded" });
+  }
+  const imageUrl = `${req.protocol}://${req.get("host")}/uploads/${
+    req.file.filename
+  }`;
+  res.status(200).json({ imageUrl });
+});
 export default router;
